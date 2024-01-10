d9a184d97966c0e000efe91351c4edb4a1b16a17
Jatin Ranpariya <jatinranpariya1510@gmail.com>
2024-01-10 13:27:18 +0530

    Mid-Line reached

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import About from "./components/About";
+import Alert from "./components/Alert";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+export default function App() {
+  const [mode, setMode] = useState("light"); // whethere dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "black";
+      showAlert("Dark Mode Enabled", "success");
+      document.title = "TextUtils - Dark Mode";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode Enabled", "success");
+      document.title = "TextUtils - Light Mode";
+    }
+  };
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Navbar
+        title="TextUtil"
+        about="About"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        <Routes>
+          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />}/>
+          <Route exact path="/about" element={<About mode={mode} />}/>
+        </Routes>
+      </div>
+    </BrowserRouter> 
   );
 }
-
-export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,159 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  
+  const [text, setText] = useState("");
+  const wordCounter = (str) => {
+    const strArr = str.split(/[ ]+/);
+    const size = strArr.length;
+    if(strArr[size - 1] === "")
+      strArr.pop();
+    return strArr.length;
+  };
+  const handleUpClick = () => {
+    setText(text.toUpperCase());
+    props.showAlert("Converted to uppercase!", "success");
+  };
+  const handleLoClick = () => {
+    setText(text.toLowerCase());
+    props.showAlert("Converted to lowercase!", "success");
+  };
+  const handleClearClick = () => {
+    setText("");
+    props.showAlert("Text Cleared!", "success");
+  };
+  function capitalizeIthCharacter(inputString, i) {
+    if (i < 0 || i >= inputString.length) {
+      props.showAlert("Invalid index!", "danger");
+      return inputString;
+    }
+    return (
+      inputString.substring(0, i) +
+      inputString[i].toUpperCase() +
+      inputString.substring(i + 1)
+    );
+  }
+  const handleCapitalClick = () => {
+    setText(text.toLowerCase());
+    console.log(text);
+    let newText = text.split(" ");
+    for (let i = 0; i < newText.length; i++) {
+      newText[i] = capitalizeIthCharacter(newText[i], 0);
+    }
+    setText(newText.join(" "));
+    props.showAlert("Text Capitalized!", "success");
+  };
+
+  function unsecuredCopyToClipboard(text) {
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    try {
+      document.execCommand("copy");
+    } catch (err) {
+      props.showAlert("Unable to copy to clipboard!", "danger");
+    }
+    document.body.removeChild(textArea);
+  }
+  const handleCopyClick = () => {
+    if (window.isSecureContext && navigator.clipboard) {
+      navigator.clipboard.writeText(text);
+    } else {
+      unsecuredCopyToClipboard(text);
+    }
+    props.showAlert("Text Copied to clipboard!", "success");
+  };
+  const handleSpeakClick = () => {
+    // Check if the SpeechSynthesis API is available in the browser
+    if ("speechSynthesis" in window) {
+      // Function to speak the provided text
+      function speak(text) {
+        // Create a SpeechSynthesisUtterance object
+        const utterance = new SpeechSynthesisUtterance(text);
+
+        // Use the default speech synthesis voice
+        const voices = window.speechSynthesis.getVoices();
+        utterance.voice = voices[0];
+
+        // Speak the text
+        window.speechSynthesis.speak(utterance);
+      }
+
+      speak(text);
+    } else {
+      console.error("Speech synthesis not supported in this browser");
+    }
+    props.showAlert("Text spoken!", "success");
+  };
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+  const handleExtraSpace = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+    props.showAlert("Removed Extra Space!", "success");
+  };
+
+  return (
+    <>
+      <div
+        className={`container text-${
+          props.mode === "light" ? "black" : "white"
+        }`}
+      >
+        <h2>{props.heading} </h2>
+        <div className="mb-3">
+          <textarea
+            style={{
+              backgroundColor: props.mode === "light" ? "white" : "#212529",
+              color: props.mode === "light" ? "black" : "white",
+            }}
+            value={text}
+            className="form-control"
+            id="myBox"
+            rows="8"
+            onChange={handleOnChange}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+          Convert to uppercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleLoClick}>
+          Convert to lowercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleCapitalClick}>
+          Capitalize
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleClearClick}>
+          Clear Text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleCopyClick}>
+          Copy to clipboard
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleSpeakClick}>
+          Speak
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleExtraSpace}>
+          Remove Extra Space
+        </button>
+      </div>
+      <div
+        className={`container my-3 text-${
+          props.mode === "light" ? "black" : "white"
+        }`}
+      >
+        <h2>Your text summary</h2>
+        <p>
+          {wordCounter(text)} words and {text.length} characters
+        </p>
+        <p>{0.008 * wordCounter(text)} minutes read</p>
+        <h2>Preview</h2>
+        <p>
+          {text.length > 0 ? text : "Enter something above to preview it here"}
+        </p>
+      </div>
+    </>
+  );
+}
